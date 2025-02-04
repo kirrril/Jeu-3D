9e65dd0313dea52b1dff212e5c4f5f88af1c73b3
WaterWhite71 <102372963+WaterWhite71@users.noreply.github.com>
2025-02-04 17:08:40 +0100

    Bug Fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,65 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody rb;
+	[SerializeField]
+	private Rigidbody rb;
 
-    [SerializeField]
-    private Camera cam;
+	[SerializeField]
+	private Camera cam;
 
-    private Vector3 inputMove = Vector3.zero;
-    private float inputRotate = 0;
+	private Vector3 inputMove = Vector3.zero;
+	private float inputRotate = 0;
 
-    [SerializeField]
-    private float forwardSpeed = 2, sideSpeed = 2, upSpeed = 2, rotationSpeed = 5, rotationCoeff = 100;
-
-
-
-    void Start()
-    {
-
-    }
+	[SerializeField]
+	private float forwardSpeed = 2, sideSpeed = 2, upSpeed = 2, rotationSpeed = 5, rotationCoeff = 100;
 
 
-    void Update()
-    {
-        GetInput();
-    }
 
-    void FixedUpdate()
-    {
-        RotatePlayer();
-        MovePlayer();
-    }
+	void Start()
+	{
 
-    void GetInput()
-    {
-        inputMove.x = Input.GetAxis("Horizontal");
-        inputMove.y = Input.GetAxis("Vertical");
-        inputMove.z = Input.GetAxis("Jump");
-        inputRotate = Input.GetAxis("Mouse X");
-    }
-
-    void RotatePlayer()
-    {
-        float playerRotation = inputRotate * rotationCoeff * rotationSpeed * Time.fixedDeltaTime;
-
-        Quaternion deltaRotation = Quaternion.Euler(0, playerRotation, 0);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-    }
+	}
 
 
-    void MovePlayer()
-    {
-        Vector3 forwardMove = transform.forward * inputMove.y * forwardSpeed;
-        Vector3 sideMove = transform.right * inputMove.x * sideSpeed;
-        Vector3 upMove = transform.up * inputMove.z * upSpeed;
+	void Update()
+	{
+		GetInput();
+	}
 
-        Vector3 resultMove = rb.rotation * forwardMove + sideMove + upMove;
+	void FixedUpdate()
+	{
+		RotatePlayer();
+		MovePlayer();
+	}
 
-        rb.velocity = resultMove;
+	void GetInput()
+	{
+		inputMove.x = Input.GetAxis("Horizontal");
+		inputMove.y = Input.GetAxis("Vertical");
+		inputMove.z = Input.GetAxis("Jump");
+		inputRotate = Input.GetAxis("Mouse X");
+	}
 
-    }
+	void RotatePlayer()
+	{
+		float playerRotation = inputRotate * rotationCoeff * rotationSpeed * Time.fixedDeltaTime;
+
+		Quaternion deltaRotation = Quaternion.Euler(0, playerRotation, 0);
+		rb.MoveRotation(rb.rotation * deltaRotation);
+	}
+
+
+	void MovePlayer()
+	{
+		Vector3 forwardMove = transform.forward * inputMove.y * forwardSpeed;
+		Vector3 sideMove = transform.right * inputMove.x * sideSpeed;
+		Vector3 upMove = transform.up * inputMove.z * upSpeed;
+
+		Vector3 resultMove = forwardMove + sideMove + upMove;
+
+		rb.MovePosition(transform.position + resultMove * Time.deltaTime);
+
+	}
 
 
 }
